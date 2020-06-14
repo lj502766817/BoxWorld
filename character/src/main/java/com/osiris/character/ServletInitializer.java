f322cdc8068543d699de39b70023d3b281e3a9a8
lijia <502766817@qq.com>
2020-06-14 16:50:25 +0800

    角色层

---
+++ character/src/main/java/com/osiris/character/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.osiris.character;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CharacterApplication.class);
+    }
+
+}
